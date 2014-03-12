3a9b0d027b117310c6eedfa86b6b83221643273b
Matheus Guilarducci <mguilarducci@gmail.com>
2014-03-11 22:09:12 -0300

    config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'development': {
+        'db': 'mongodb://localhost/muncci_devlopment'
+    },
+    'test': {
+        'db': 'mongodb://localhost/muncci_test'
+    },
+    'production': {}
+}
