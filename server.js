9978391e5495cb93d771016b35267218cf2227b9
Matheus Guilarducci <mguilarducci@gmail.com>
2014-03-11 21:25:44 -0300

    hello world

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req, res) {
+    res.send("Hello!")
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log("Running on port " + port);
+
 module.exports = app
--- test/test-app.js
+++ test/test-app.js
@@ -1,9 +1,20 @@
-var should = require('should')
+var should = require('should'),
+    request = require('supertest')
+
+
 var app = require('../server.js')
 
 describe('app', function() {
     it('should exist', function(done) {
         should.exist(app)
         done()
+    })
+
+    describe('routes', function() {
+        it('GET / should return status code 200', function(done) {
+            request(app)
+                .get('/')
+                .expect(200, done)
+        })
     })
 })
